6bbb28caa42faa08aea59835f504f22ed9d98ed6
Bohdan Shelest <b.shelest@ukr.net>
2020-12-31 19:50:05 +0200

    added mixitup, fixed css header

--- app/js/main.js
+++ app/js/main.js
@@ -1,9 +1,11 @@
 $(function () {
+  var mixer = mixitup('.week__content'); 
   $('.top-slider__inner').slick({
     dots: true,
     arrows: false,
     fade:true,
     autoplay:true,
     autoplaySpeed:2000
-  });  
+  });
+
 });
