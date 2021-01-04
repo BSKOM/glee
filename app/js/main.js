92852f55bbd6eb4dd99afbc48312c34038c28270
Bohdan Shelest <b.shelest@ukr.net>
2021-01-04 20:18:56 +0200

    added category gallery (without js)

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,18 @@
 $(function () {
-  var mixer = mixitup('.week__content'); 
+  var mixer = mixitup('.week__content');
+  // function initFilters() {
+  //   $('.week__content').mixItUp({
+  //     selectors: {
+  //       filter: '.filter1',
+  //     }
+  //   });
+  //   $('.category__content').mixItUp({
+  //     selectors: {
+  //       filter: '.filter2',
+  //      }
+  //   });
+  // }
+  // $initFilters();
   $('.top-slider__inner').slick({
     dots: true,
     arrows: false,
@@ -7,5 +20,4 @@ $(function () {
     autoplay:true,
     autoplaySpeed:2000
   });
-
 });
