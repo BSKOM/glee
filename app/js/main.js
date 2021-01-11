1359c2979d46d0d0338d5bebefba67160006fc7a
Bohdan Shelest <b.shelest@ukr.net>
2021-01-11 22:14:09 +0200

    added left-side filters, added rateyo plugin

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,23 @@
 $(function () {
-  $('.js-range-slider').ionRangeSlider({ prefix: "$", hide_from_to: true,});
+  $('.js-range-slider').ionRangeSlider({ 
+    prefix: "$",
+    onStart: function (data){
+      $('.filter-price__from').text(data.from);
+      $('.filter-price__to').text(data.to);
+    }, 
+    onChange: function (data) {
+      $('.filter-price__from').text(data.from);
+      $('.filter-price__to').text(data.to);
+    },
+    hide_from_to: true,});
+
+  $(".star").rateYo({
+    rating: 3.6,
+    starWidth: "11px",
+    normalFill: "#d6d6d6",
+    ratedFill: "#ffcc00",
+    readOnly: true
+  });
 
   var containerEl1 = document.querySelector('[data-ref="container-1"]');
   var containerEl2 = document.querySelector('[data-ref="container-2"]');
@@ -10,9 +28,7 @@ $(function () {
   };
   var mixer1 = mixitup(containerEl1, config);
   var mixer1 = mixitup(containerEl2, config);
-  
-
-
+ 
   $('.top-slider__inner').slick({
     dots: true,
     arrows: false,
@@ -20,5 +36,6 @@ $(function () {
     autoplay:true,
     autoplaySpeed:2000
   });
+
 
 });
