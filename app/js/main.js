6adc8e86498f00ebf8e777f8d30609264a1843e4
Bohdan Shelest <b.shelest@ukr.net>
2021-01-19 22:32:17 +0200

    added page product-detalis, added slider, added js function for slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,21 @@
 $(function () {
+
+  $('.product-slide__thumb').slick({
+    asNavFor: '.product-slide__big',
+    focusOnSelect: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    vertical: true,
+    draggable: false
+  });
+
+  $('.product-slide__big').slick({
+    asNavFor: '.product-slide__thumb',
+    draggable: false,
+    arrows: false,
+    fade: true
+  });
+
   $('.js-range-slider').ionRangeSlider({ 
     prefix: "$",
     onStart: function (data){
